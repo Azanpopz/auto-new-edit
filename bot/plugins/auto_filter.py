2d7f8a950f19a770486d6dfcc9d25730b61c5809
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-08-27 19:54:03 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -127,7 +127,7 @@ async def auto_filter(bot, update):
             
             results.append(
                 [
-                    InlineKeyboardButton(f"⏯️button_text, url=file_link)
+                    InlineKeyboardButton(f"⏯️ button_text, url=file_link)
                 ]
             )
         
