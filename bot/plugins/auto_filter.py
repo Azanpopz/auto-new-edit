cce277c8cffdbc7a08b538c01139385043209b11
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-08-27 18:25:27 +0530

    Munipopz enthokkeyo paranj njan cheyth... 😴

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -60,6 +60,11 @@ async def auto_filter(bot, update):
     filters = await db.get_filters(group_id, query)
     
     if filters:
+        results.append(
+                [
+                    InlineKeyboardButton("🔘 JOIN OUR MAIN CHANNEL 🔘", url="https://t.me/mazhatthullikal")
+                ]
+            )
         for filter in filters: # iterating through each files
             file_name = filter.get("file_name")
             file_type = filter.get("file_type")
@@ -121,8 +126,8 @@ async def auto_filter(bot, update):
                 file_link = f"https://t.me/{bot_.username}?start={unique_id}"
             
             results.append([
-            InlineKeyboardButton(file_names, url=file_link),
-            InlineKeyboardButton(file_size, url=file_link)
+            InlineKeyboardButton(📂 file_names, url=file_link),
+            InlineKeyboardButton(📂 file_size, url=file_link)
         ])
         
     else:
