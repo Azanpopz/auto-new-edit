6d2cd1d3c593c073de5a490d012a94657a83de61
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-08-27 15:57:48 +0530

    😉

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -60,6 +60,12 @@ async def auto_filter(bot, update):
     filters = await db.get_filters(group_id, query)
     
     if filters:
+        results.append(
+                [
+                    InlineKeyboardButton("🔘 JOIN OUR MAIN CHANNEL 🔘", url="https://t.me/mazhatthullikal")
+                ]
+            )
+    if filters:
         for filter in filters: # iterating through each files
             file_name = filter.get("file_name")
             file_type = filter.get("file_type")
